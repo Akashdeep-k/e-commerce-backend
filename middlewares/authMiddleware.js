feca05c26a4205f929edf1e6bb4b6e2541c2adf7
Akash <iamakashkataria18@gmail.com>
2023-09-08 00:03:24 +0530

    add middleware for verifying token and role field of user model

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken")
+const asyncHandler = require("express-async-handler")
+const User = require("../models/user.js")
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded_token = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decoded_token.id })
+
+    if (!user) {
+        throw new Error("Please authenticate");
+    }
+
+    req.user = user;
+    next();
+})
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+    if(req.user.role !== 'admin'){
+        throw new Error("You are not admin")
+    }
+    next();
+})
+
+module.exports = { authMiddleware, isAdmin }
