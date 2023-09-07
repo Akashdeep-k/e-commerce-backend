70267829ac099112f402a3700a2f35693881988c
Akash <iamakashkataria18@gmail.com>
2023-09-08 00:49:03 +0530

    added cookie-parser middleware

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ require("dotenv").config()
 const dbConnect = require("./config/db/connect.js")
 const { notFound, errorHandler } = require("./middlewares/errorHandler.js")
 const authRouter = require("./routes/auth.js")
+const cookieParser = require("cookie-parser")
 
 dbConnect()
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/user", authRouter)
 
 app.use(notFound)
