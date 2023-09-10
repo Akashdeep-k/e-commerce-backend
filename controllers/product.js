12228760e9ee264a5df8855f9d6692dd145340f9
Akash <iamakashkataria18@gmail.com>
2023-09-10 15:15:43 +0530

    Implement filtering options for Get all products

--- controllers/product.js
+++ controllers/product.js
@@ -24,7 +24,16 @@ const getAProduct = asyncHandler(async (req, res) => {
 
 const getAllProducts = asyncHandler(async (req, res) => {
     try {
-        const products = await Product.find()
+        //Filtering
+        const filterObj = { ...req.query }
+        const excludeQuery = ["page", "limit", "sort", "fields"]
+        excludeQuery.forEach(query => delete filterObj[query])
+
+        let queryStr = JSON.stringify(filterObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+        let query = Product.find(JSON.parse(queryStr))
+
+        const products = await query
         res.json(products)
     } catch (e) {
         throw new Error(e)
@@ -34,7 +43,7 @@ const getAllProducts = asyncHandler(async (req, res) => {
 const updateAProduct = asyncHandler(async (req, res) => {
     const { id } = req.params
     try {
-        if(req.body.title){
+        if (req.body.title) {
             req.body.slug = slugify(req.body.title)
         }
         const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
